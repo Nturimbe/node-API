962498ef3a15a8cc488f74640e4f8214d59148ec
Nturimbe <151768517+Nturimbe@users.noreply.github.com>
2023-12-17 22:26:03 +0000

    final files

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
 app.get('/', (req, res)=>{
   res.send('Hello, my node API is running on port 3000')
 })
 
-app.listen(3000, ()=>{
-  console.log('My API app is running on port 3000')
+
+app.get('/blog', (req, res)=>{
+  res.send('Hello, my  blog is available for public reading on port 3000. nodemon has been added to it.')
 })
+
+//mongoose.set('strictly', false)
+mongoose.connect('mongodb+srv://Nturimbe:Nturimbe@gabelapi.10ejsqy.mongodb.net/?retryWrites=true&w=majority')
+.then(() => {
+  app.listen(3000, ()=>{
+    console.log('My API app is running on port 3000')
+  })
+  console.log('Connected to MongoDB')
+}).catch((error) => {
+    console.log('error!')
+  })
